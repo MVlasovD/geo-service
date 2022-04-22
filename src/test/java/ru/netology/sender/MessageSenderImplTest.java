00fed74b1068aa0821cc43a0f76097c9c1b4bb7d
Mikhail Vlasov <m.vlasov@dunice.net>
2022-04-22 17:36:47 +0300

    tests begin

---
+++ src/test/java/ru/netology/sender/MessageSenderImplTest.java
@@ -0,0 +1,20 @@
+package ru.netology.sender;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import ru.netology.geo.GeoServiceImpl;
+import ru.netology.i18n.LocalizationServiceImpl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageSenderImplTest {
+
+
+    @Test
+    void send() {
+    GeoServiceImpl geoServiceMock = Mockito.mock(GeoServiceImpl.class);
+    LocalizationServiceImpl localizationServiceMock = Mockito.mock(LocalizationServiceImpl.class);
+
+
+    }
+}
