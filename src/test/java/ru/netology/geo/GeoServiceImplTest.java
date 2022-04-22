aa0a79ddae62ef6c3f44455f5898442bd6152e11
Mikhail Vlasov <m.vlasov@dunice.net>
2022-04-22 17:36:47 +0300

    tests begin

---
+++ src/test/java/ru/netology/geo/GeoServiceImplTest.java
@@ -0,0 +1,14 @@
+package ru.netology.geo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GeoServiceImplTest {
+
+    @Test
+    void byIp() {
+
+
+    }
+}
