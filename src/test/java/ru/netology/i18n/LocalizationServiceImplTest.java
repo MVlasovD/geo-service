5e1205165ff441bf5b59dc3c03a9e7f79e859084
Mikhail Vlasov <m.vlasov@dunice.net>
2022-04-27 17:37:58 +0300

    tests done

--- src/test/java/ru/netology/i18n/LocalizationServiceImplTest.java
+++ src/test/java/ru/netology/i18n/LocalizationServiceImplTest.java
@@ -1,5 +1,6 @@
 package ru.netology.i18n;
 
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import ru.netology.entity.Country;
@@ -10,12 +11,13 @@ import static ru.netology.entity.Country.RUSSIA;
 class LocalizationServiceImplTest {
 
     @Test
+    @Order(1)
     void locale() {
         Country country = RUSSIA;
         String expected = "Добро пожаловать";
         assertNotNull(country);
-        LocalizationServiceImpl localizationServiceMock = Mockito.mock(LocalizationServiceImpl.class);
-        Mockito.when(localizationServiceMock.locale(country))
-                .thenReturn("Добро пожаловать");
+        LocalizationServiceImpl loc = new LocalizationServiceImpl();
+        String result = loc.locale(country);
+        assertEquals(expected, result);
     }
 }
