588369507fc5da52e82358ca76dacb44dfe8740d
Vlasov Mikhail <m.vlasov@dunice.net>
2022-04-28 20:54:14 +0300

    mock 270422

--- src/test/java/ru/netology/sender/MessageSenderImplTest.java
+++ src/test/java/ru/netology/sender/MessageSenderImplTest.java
@@ -9,17 +9,24 @@ import ru.netology.geo.GeoServiceImpl;
 import ru.netology.i18n.LocalizationService;
 import ru.netology.i18n.LocalizationServiceImpl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class MessageSenderImplTest {
 
     @Test
     void send() {
-        GeoService geoServiceMock = Mockito.mock(GeoServiceImpl.class);
-        LocalizationService localizationServiceMock = Mockito.mock(LocalizationServiceImpl.class);
+        GeoService geoServiceMock = Mockito.spy(GeoServiceImpl.class);
+        LocalizationService localizationServiceMock = Mockito.spy(LocalizationServiceImpl.class);
+//        MessageSender messageSenderMock = new MessageSenderImpl(geoServiceMock, localizationServiceMock);
+        MessageSender messageSenderMock = Mockito.mock(MessageSenderImpl.class);
+        String ipAddress = "172.123.12.19";
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "172.123.12.19");
+        Location location = new Location("Moscow", Country.RUSSIA, null, 0);
+        String country = localizationServiceMock.locale(Country.RUSSIA);
+        Mockito.when(messageSenderMock.send(headers))
+                .thenReturn("RUSSIA");
 
-        Mockito.when(geoServiceMock.byIp("172.123.12.19"))
-                .thenReturn(new Location("Moscow",Country.RUSSIA,null, 0));
-
-        Mockito.when(localizationServiceMock.locale(Country.RUSSIA))
-                .thenReturn("Добро пожаловать");
     }
 }
