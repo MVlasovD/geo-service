5daf3af8cee66a68c0652ec18c6fb78acd56932b
Mikhail Vlasov <m.vlasov@dunice.net>
2022-04-28 09:38:33 +0300

    tests and mocks done

--- src/test/java/ru/netology/sender/MessageSenderImplTest.java
+++ src/test/java/ru/netology/sender/MessageSenderImplTest.java
@@ -18,13 +18,11 @@ class MessageSenderImplTest {
     void send() {
         GeoService geoServiceMock = Mockito.mock(GeoServiceImpl.class);
         LocalizationService localizationServiceMock = Mockito.mock(LocalizationServiceImpl.class);
-//        MessageSender messageSenderMock = new MessageSenderImpl(geoServiceMock, localizationServiceMock);
-//        String ipAddress = "172.123.12.19";
-//        Map<String, String> headers = new HashMap<String, String>();
-//        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "172.123.12.19");
-//        Location location = new Location("Moscow", Country.RUSSIA, null, 0);
-//        String country = localizationServiceMock.locale(Country.RUSSIA);
+
         Mockito.when(geoServiceMock.byIp("172.123.12.19"))
-                .thenReturn(localizationServiceMock.locale(loc));
+                .thenReturn(new Location("Moscow",Country.RUSSIA,null, 0));
+
+        Mockito.when(localizationServiceMock.locale(Country.RUSSIA))
+                .thenReturn("Добро пожаловать");
     }
 }
