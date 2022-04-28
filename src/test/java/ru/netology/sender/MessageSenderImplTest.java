c7daaf23626960adeeaf806b705c330ecf376b3a
Vlasov Mikhail <m.vlasov@dunice.net>
2022-04-28 20:54:14 +0300

    mocks done

--- src/test/java/ru/netology/sender/MessageSenderImplTest.java
+++ src/test/java/ru/netology/sender/MessageSenderImplTest.java
@@ -1,5 +1,6 @@
 package ru.netology.sender;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import ru.netology.entity.Country;
@@ -9,17 +10,29 @@ import ru.netology.geo.GeoServiceImpl;
 import ru.netology.i18n.LocalizationService;
 import ru.netology.i18n.LocalizationServiceImpl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class MessageSenderImplTest {
 
     @Test
     void send() {
-        GeoService geoServiceMock = Mockito.mock(GeoServiceImpl.class);
-        LocalizationService localizationServiceMock = Mockito.mock(LocalizationServiceImpl.class);
+        GeoService geoServiceMock = Mockito.spy(GeoServiceImpl.class);
+        LocalizationService localizationServiceMock = Mockito.spy(LocalizationServiceImpl.class);
+        MessageSender messageSenderMock = new MessageSenderImpl(geoServiceMock, localizationServiceMock);
+        String ipAddressR = "172.";
+        String ipAddressO = "96.";
+        String expectedR = "Добро пожаловать";
+        String expectedO = "Welcome";
+        Map<String, String> headersR = new HashMap<String, String>();
+        Map<String, String> headersO = new HashMap<String, String>();
+        headersR.put(MessageSenderImpl.IP_ADDRESS_HEADER,ipAddressR);
+        headersO.put(MessageSenderImpl.IP_ADDRESS_HEADER,ipAddressO);
+        String actualR = messageSenderMock.send(headersR);
+        System.out.println();
+        String actualO = messageSenderMock.send(headersO);
 
-        Mockito.when(geoServiceMock.byIp("172.123.12.19"))
-                .thenReturn(new Location("Moscow",Country.RUSSIA,null, 0));
-
-        Mockito.when(localizationServiceMock.locale(Country.RUSSIA))
-                .thenReturn("Добро пожаловать");
+        Assertions.assertEquals(expectedR,actualR);
+        Assertions.assertEquals(expectedO,actualO);
     }
 }
